1013afe84c5c6406c2382ce11c8bdd13a1dd3267
Curtis Higa <curtis.higa@gmail.com>
2018-06-15 21:01:58 -0700

    Creates Data_Story report

--- Data_Story/data_vis.py
+++ Data_Story/data_vis.py
@@ -38,30 +38,26 @@ bayarea = grouped[bayarea_counties]
 bayarea['bay'] = bayarea.mean(axis=1)
 
 
-## import full zillow research data
-#full_zillow = pd.read_csv('Zip_Zhvi_AllHomes.csv',
-#                          index_col = 'RegionName')
-#
-#
-## import price per square foot of all zips in u.s.
-#price_per_sqft = pd.read_csv('Zip_MedianListingPricePerSqft_AllHomes.csv',
-#                             index_col = 'RegionName')
-#
-#
-## add column to price_per_sqft indicating which zips are in the bay area
-##price_per_sqft['in_bayarea'] = price_per_sqft['CountyName'].isin(bayarea_counties)
-#
-#
-## pivot price_per_sqft to aggregate avg per county
-#avg_price_sqft_county = price_per_sqft.pivot_table(index = 'CountyName',
-#                                                   values = price_per_sqft.loc[:,'2010-01':].columns,
-#                                                   aggfunc = 'mean')
-#
-#avg_price_sqft_county = avg_price_sqft_county.transpose()
-#
-#plt.plot(avg_price_sqft_county[bayarea_counties])
-#
-#
-#ba_zip_avg = combined.pivot_table(index = combined.loc[:,'1996-04-01':'2018-04-01'].columns,
-#                                  columns = 'CountyName',
-#                                  aggfunc = 'mean')
+
+# import price per square foot of all zips in u.s.
+price_per_sqft = pd.read_csv('Zip_MedianListingPricePerSqft_AllHomes.csv',
+                             index_col = 'RegionName')
+
+
+# add column to price_per_sqft indicating which zips are in the bay area
+#price_per_sqft['in_bayarea'] = price_per_sqft['CountyName'].isin(bayarea_counties)
+
+
+# pivot price_per_sqft to aggregate avg per county
+avg_price_sqft_county = price_per_sqft.pivot_table(index = 'CountyName',
+                                                   values = price_per_sqft.loc[:,'2010-01':].columns,
+                                                   aggfunc = 'mean')
+
+avg_price_sqft_county = avg_price_sqft_county.transpose()
+
+plt.plot(avg_price_sqft_county[bayarea_counties])
+
+
+ba_zip_avg = combined.pivot_table(index = combined.loc[:,'1996-04-01':'2018-04-01'].columns,
+                                  columns = 'CountyName',
+                                  aggfunc = 'mean')
