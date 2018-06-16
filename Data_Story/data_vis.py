1b244378a2b49a3dac7ec94561a1dee478cddb25
Curtis Higa <curtis.higa@gmail.com>
2018-06-16 02:10:46 -0700

    Continues Data_Story.ipynb report and data_vis.py

--- Data_Story/data_vis.py
+++ Data_Story/data_vis.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from datetime import datetime
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 
@@ -25,39 +26,41 @@ grouped.index = pd.to_datetime(grouped.index)
 bayarea_counties = ['Alameda', 'Contra Costa', 'Marin', 'Napa',
                     'San Francisco', 'San Mateo', 'Santa Clara',
                     'Solano', 'Sonoma']
-i = 1
-while i <= len(bayarea_counties):
-    plt.subplot(3, 3, i)
-    plt.plot(grouped[bayarea_counties[i-1]], label = bayarea_counties[i-1])
-    i += 1
-plt.axis([1996,2019,0,2000000])    
-plt.show()    
+#i = 1
+#while i <= len(bayarea_counties):
+#    plt.subplot(3, 3, i)
+#    plt.plot(grouped[bayarea_counties[i-1]], label = bayarea_counties[i-1])
+#    i += 1
+#plt.axis([1996,2019,0,2000000])    
+#plt.show()    
         
 
 bayarea = grouped[bayarea_counties]
 bayarea['bay'] = bayarea.mean(axis=1)
 
 
+sns.stripplot(data = grouped.loc['2018-04',:].transpose())
+sns.stripplot(data = bayarea.loc['2018-04',:].transpose(), color = 'red')
 
 # import price per square foot of all zips in u.s.
-price_per_sqft = pd.read_csv('Zip_MedianListingPricePerSqft_AllHomes.csv',
-                             index_col = 'RegionName')
+price_per_sqft = pd.read_csv('Zip_MedianListingPricePerSqft_AllHomes.csv')
 
 
-# add column to price_per_sqft indicating which zips are in the bay area
-#price_per_sqft['in_bayarea'] = price_per_sqft['CountyName'].isin(bayarea_counties)
-
-
-# pivot price_per_sqft to aggregate avg per county
-avg_price_sqft_county = price_per_sqft.pivot_table(index = 'CountyName',
-                                                   values = price_per_sqft.loc[:,'2010-01':].columns,
-                                                   aggfunc = 'mean')
-
-avg_price_sqft_county = avg_price_sqft_county.transpose()
-
-plt.plot(avg_price_sqft_county[bayarea_counties])
-
-
-ba_zip_avg = combined.pivot_table(index = combined.loc[:,'1996-04-01':'2018-04-01'].columns,
-                                  columns = 'CountyName',
-                                  aggfunc = 'mean')
+sns.tsplot(bayarea['Santa Clara'])
+## add column to price_per_sqft indicating which zips are in the bay area
+##price_per_sqft['in_bayarea'] = price_per_sqft['CountyName'].isin(bayarea_counties)
+#
+#
+## pivot price_per_sqft to aggregate avg per county
+#avg_price_sqft_county = price_per_sqft.pivot_table(index = 'CountyName',
+#                                                   values = price_per_sqft.loc[:,'2010-01':].columns,
+#                                                   aggfunc = 'mean')
+#
+#avg_price_sqft_county = avg_price_sqft_county.transpose()
+#
+#plt.plot(avg_price_sqft_county[bayarea_counties])
+#
+#
+#ba_zip_avg = combined.pivot_table(index = combined.loc[:,'1996-04-01':'2018-04-01'].columns,
+#                                  columns = 'CountyName',
+#                                  aggfunc = 'mean')
