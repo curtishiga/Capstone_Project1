8014be459fbbbbda6fc5895fe913f08b0a99bc35
Curtis Higa <curtis.higa@gmail.com>
2018-09-10 11:44:43 -0700

    Adds final code, final report, and starts final presentation

--- Data_Story/data_vis.py
+++ Data_Story/data_vis.py
@@ -4,84 +4,54 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-
 # import by_zip and transform it
 full = pd.read_csv('C:\\Users\\Curtis\\Desktop\\Springboard\\Capstone_Project1\\data\\Zip_Zhvi_AllHomes.csv',
                    parse_dates = True)
 
-
 del full['RegionID'], full['SizeRank'], full['Metro'], full['City']
 
-# full = full.set_index(['State', 'CountyName', 'RegionName'])
-
-#full.index.name = 'Date'
-#
-#full = full.reset_index()
-#
-grouped = full.groupby('CountyName').mean().transpose()
-grouped = grouped.iloc[1:,:]
-grouped.index = pd.to_datetime(grouped.index)
+grouped = full.groupby(['State', 'CountyName']).mean()
+grouped = grouped.iloc[:,1:]
+grouped.columns = pd.to_datetime(grouped.columns)
 
 # create a list of bay area counties
 bayarea_counties = ['Alameda', 'Contra Costa', 'Marin', 'Napa',
                     'San Francisco', 'San Mateo', 'Santa Clara',
                     'Solano', 'Sonoma']
-#i = 1
-#while i <= len(bayarea_counties):
-#    plt.subplot(3, 3, i)
-#    plt.plot(grouped[bayarea_counties[i-1]], label = bayarea_counties[i-1])
-#    i += 1
-#plt.axis([1996,2019,0,2000000])    
-#plt.show()    
+
+for county in bayarea_counties:
+    plt.plot(grouped.loc[('CA', county),:])
+    plt.title(county)
+    plt.xlabel('Time')
+    plt.ylabel('Price')
+    plt.show()    
         
 
-bayarea = grouped[bayarea_counties]
-bayarea['bay'] = bayarea.mean(axis=1)
+bayarea = grouped.loc['CA',:]
+bayarea = bayarea.loc[bayarea.index.isin(bayarea_counties),:]
 
 
-#sns.stripplot(data = grouped.loc['2018-04',:].transpose())
-#sns.stripplot(data = bayarea.loc['2018-04',:].transpose(), color = 'red')
+sns.stripplot(data = grouped.loc[:,'2018-04-01'])
+sns.stripplot(data = bayarea.loc[:,'2018-04-01'], color = 'red')
 
 # import price per square foot of all zips in u.s.
-ppsqft = pd.read_csv('Zip_MedianListingPricePerSqft_AllHomes.csv')
+ppsqft = pd.read_csv('C:\\Users\\Curtis\\Desktop\\Springboard\\Capstone_Project1\\data\\Zip_MedianListingPricePerSqft_AllHomes.csv')
 
 del ppsqft['SizeRank'], ppsqft['Metro'], ppsqft['City']
 
-ppsqft_grouped = ppsqft.groupby('CountyName').mean().transpose()
-ppsqft_grouped = ppsqft_grouped.iloc[1:,:]
-ppsqft_grouped.index = pd.to_datetime(ppsqft_grouped.index)
+ppsqft_grouped = ppsqft.groupby('CountyName').mean()
+ppsqft_grouped = ppsqft_grouped.iloc[:, 1:]
+ppsqft_grouped.columns = pd.to_datetime(ppsqft_grouped.columns)
 
 
-pct_change = pd.concat([grouped['2010-01'], grouped['2018-04']]).transpose()
+pct_change = pd.concat([grouped['2010-01-01'], grouped['2018-04-01']], axis = 1)
 
 pct_change['change'] = (pct_change.iloc[:,1] - pct_change.iloc[:,0])/(pct_change.iloc[:,0])
 
 
-ppsqft_pct_change = pd.concat([ppsqft_grouped['2010-01'], ppsqft_grouped['2018-04']]).transpose()
+ppsqft_pct_change = pd.concat([ppsqft_grouped['2010-01-01'], ppsqft_grouped['2018-04-01']], axis = 1)
 
 ppsqft_pct_change['change'] = (ppsqft_pct_change.iloc[:,1] - ppsqft_pct_change.iloc[:,0])/(ppsqft_pct_change.iloc[:,0])
 
 sns.swarmplot(pct_change['change'], orient = 'v')
 sns.swarmplot(pct_change['change'][bayarea_counties], orient = 'v', color = 'red')
-
-
-gt = grouped.transpose()
-ppsf_gt = ppsqft_grouped.transpose()
-
-## add column to price_per_sqft indicating which zips are in the bay area
-##price_per_sqft['in_bayarea'] = price_per_sqft['CountyName'].isin(bayarea_counties)
-#
-#
-## pivot price_per_sqft to aggregate avg per county
-#avg_price_sqft_county = price_per_sqft.pivot_table(index = 'CountyName',
-#                                                   values = price_per_sqft.loc[:,'2010-01':].columns,
-#                                                   aggfunc = 'mean')
-#
-#avg_price_sqft_county = avg_price_sqft_county.transpose()
-#
-#plt.plot(avg_price_sqft_county[bayarea_counties])
-#
-#
-#ba_zip_avg = combined.pivot_table(index = combined.loc[:,'1996-04-01':'2018-04-01'].columns,
-#                                  columns = 'CountyName',
-#                                  aggfunc = 'mean')
